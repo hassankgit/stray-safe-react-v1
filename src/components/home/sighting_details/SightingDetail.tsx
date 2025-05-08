4849998926cfd3ba49b96af1ff4d835010e360fe
hk6562004 <hk656.2004@gmail.com>
2025-05-07 20:00:22 -0400

    fixing sidebar sizing bug, extracted sightingdetail info, added buttonprimary, added label and sublabel to textarea, new colors,

--- src/components/home/sighting_details/SightingDetail.tsx
+++ src/components/home/sighting_details/SightingDetail.tsx
@@ -5,9 +5,33 @@ import Heading4 from "@/components/headings/heading4/Heading4";
 import Tag from "@/components/tag/Tag";
 import InformationItem from "@/components/information/item/InformationItem";
 import TextArea from "@/components/input/custom_input/TextArea";
+import ButtonPrimary from "@/components/input/button_primary/ButtonPrimary";
 
 type SightingDetailProps = {
   className: string;
+};
+
+const sightingDetails = {
+  name: "Mano",
+  species: "cat",
+  breed: "tabby",
+  age: "6-12 months",
+  sex: "female",
+  whenSpotted: "spotted april 16, 5:55 pm",
+  location: "100 E Penn Square, Philadelphia, PA, 19104",
+  tags: ["still roaming", "friendly", "healthy"],
+  notes:
+    "friendly neighborhood cat mano that runs up and down the block this manos block be sure to feedher when u run by ykwis ong magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velitesse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpaqui officia deserunt mollit anim id est laborum.",
+  sightedBy: "straysafeuser1",
+};
+
+// TODO move elsewhere
+const tagColors: { [key: string]: string } = {
+  "still roaming": "yellow",
+  friendly: "green",
+  healthy: "green",
+  female: "pink",
+  male: "blue",
 };
 
 export default function SightingDetail(props: SightingDetailProps) {
@@ -18,38 +42,49 @@ export default function SightingDetail(props: SightingDetailProps) {
         <div className={styles.information}>
           <div className={styles.heading}>
             <div className={styles.title}>
-              <Heading1 text={"Mano"} />
-              <Heading1 text={"cat - tabby"} />
+              <Heading1 text={sightingDetails.name} />
+              <Heading1
+                text={`${sightingDetails.species} - ${sightingDetails.breed}`}
+              />
             </div>
-            <Heading4 text={"spotted april 16, 5:55 pm"} />
+            <Heading4 text={sightingDetails.whenSpotted} />
             <div className={styles.location_wrapper}>
               <FaMapMarkerAlt className={styles.location_icon} />
-              <Heading4
-                text={"around 100 E Penn Square, Philadelphia, PA, 19104"}
-              />
+              <Heading4 text={sightingDetails.location} />
             </div>
           </div>
           <div className={styles.tag_section}>
             <Heading4 text={"this animal is..."} />
+
             <div className={styles.tag_wrapper}>
-              <Tag text="still roaming" color="yellow" />
-              <Tag text="friendly" color="green" />
-              <Tag text="healthy" color="green" />
+              {sightingDetails.tags.map((text, index) => (
+                <Tag key={index} text={text} color={tagColors[text]} />
+              ))}
             </div>
           </div>
           <div className={styles.information_table}>
             <Heading4 text="information:" />
             <div className={styles.information_wrapper}>
-              <InformationItem label="species" tagLabel="cat" />
-              <InformationItem label="breed" tagLabel="tabby" />
-              <InformationItem label="age" tagLabel="6-12 months" />
-              <InformationItem label="sex" tagLabel="female" tagColor="pink" />
+              <InformationItem
+                label="species"
+                tagLabel={sightingDetails.species}
+              />
+              <InformationItem label="breed" tagLabel={sightingDetails.breed} />
+              <InformationItem label="age" tagLabel={sightingDetails.age} />
+              <InformationItem
+                label="sex"
+                tagLabel={sightingDetails.sex}
+                tagColor={tagColors[sightingDetails.sex]}
+              />
             </div>
           </div>
           <TextArea
+            label={`notes from ${sightingDetails.sightedBy}: `}
             className={styles.notes}
-            text="friendly neighborhood cat mano that runs up and down the block this mano’s block be sure to feed her when u run by ykwis ong magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            text={sightingDetails.notes}
           />
+          <Heading4 text={`sighted by ${sightingDetails.sightedBy}: `} />
+          <ButtonPrimary type="submit" label="contact sighter" />
         </div>
       </div>
     </div>
--- src/components/input/custom_input/TextArea.tsx
+++ src/components/input/custom_input/TextArea.tsx
@@ -1,12 +1,21 @@
+import Heading4 from "@/components/headings/heading4/Heading4";
 import styles from "./TextArea.module.scss";
 
 type TextAreaProps = {
   className?: string;
+  label?: string;
+  sublabel?: string;
   text: string;
 };
 
 export default function TextArea(props: TextAreaProps) {
   return (
-    <p className={`${styles.text_area} ${props.className}`}>{props.text}</p>
+    <div className={styles.text_area_wrapper}>
+      {props.label && <Heading4 text={props.label} />}
+      <p className={`${styles.text_area} ${props.className}`}>{props.text}</p>
+      {props.sublabel && (
+        <p className={styles.text_area_sublabel}>{props.sublabel}</p>
+      )}
+    </div>
   );
 }
